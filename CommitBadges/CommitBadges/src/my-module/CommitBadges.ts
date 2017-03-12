2d86a5ead3c2b1c3ad435b3b2edf76187d27981e
Zabaa <zaba.luk@gmail.com>
2017-03-12 19:24:36 +0100

    grid maker

---
+++ CommitBadges/CommitBadges/src/my-module/CommitBadges.ts
@@ -0,0 +1,29 @@
+﻿/// <reference types="vss-web-extension-sdk" />
+
+import Controls = require("VSS/Controls");
+import Grids = require("VSS/Controls/Grids");
+
+export class CommitBadges {
+    gridContainer = $("#grid_container");
+
+    public buildGrid() {
+        var gridOptions: Grids.IGridOptions = {
+            height: "100%",
+            width: "100%",
+            source: function () {
+                var result = [], i;
+                for (i = 0; i < 100; i++) {
+                    result[result.length] = [i, "Column 2 text" + i, "Column 3 " + Math.random()];
+                }
+
+                return result;
+            }(),
+            columns: [
+                { text: "Column 1", index: 0, width: 50 },
+                { text: "Column 2", index: 1, width: 200, canSortBy: false },
+                { text: "Column 3", index: 2, width: 450 }]
+        };
+
+        Controls.create(Grids.Grid, this.gridContainer, gridOptions);
+    }
+}
--- CommitBadges/CommitBadges/src/my-module/app.ts
+++ CommitBadges/CommitBadges/src/my-module/app.ts
@@ -1,25 +1,10 @@
-﻿/// <reference types="vss-web-extension-sdk" />
+﻿import CommitBadges = require("./CommitBadges");
 
-import Controls = require("VSS/Controls");
-import Grids = require("VSS/Controls/Grids");
+VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
+    var commitBadges = new CommitBadges.CommitBadges();
+    commitBadges.buildGrid();
 
-var container = $("#sample-container");
+    VSS.notifyLoadSucceeded();
+});
 
-var gridOptions: Grids.IGridOptions = {
-    height: "100%",
-    width: "100%",
-    source: function () {
-        var result = [], i;
-        for (i = 0; i < 100; i++) {
-            result[result.length] = [i, "Column 2 text" + i, "Column 3 " + Math.random()];
-        }
 
-        return result;
-    }(),
-    columns: [
-        { text: "Column 1", index: 0, width: 50 },
-        { text: "Column 2", index: 1, width: 200, canSortBy: false },
-        { text: "Column 3", index: 2, width: 450 }]
-};
-
-Controls.create(Grids.Grid, container, gridOptions);
