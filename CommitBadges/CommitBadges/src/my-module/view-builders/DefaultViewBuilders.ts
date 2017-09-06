3d0bd783e048167e9efe28f4da70a1acc79e3ece
Zabaa <zaba.luk@gmail.com>
2017-09-06 20:25:25 +0200

    checnge view builder

--- CommitBadges/CommitBadges/src/my-module/view-builders/DefaultViewBuilders.ts
+++ CommitBadges/CommitBadges/src/my-module/view-builders/DefaultViewBuilders.ts
@@ -9,22 +9,24 @@ export class DefaultViewBuilder implements ViewBuilder {
     public build(badge: Badge) {
 
         let gridContainer = $(`#${badge.constructor.name}`);
+        let progress: string = `<progress max="${badge.requiredPoints}" value="${badge.points}"></progress>`;
 
         var source = [
             { key: "Name", value: badge.name },
             { key: "Description", value: badge.description },
             { key: "Point", value: badge.points },
-            { key: "Required", value: badge.requiredPoints }
+            { key: "Required", value: badge.requiredPoints },
+            { key: "Progress", value: progress}
         ];
 
         var gridOptions: Grids.IGridOptions = {
-            height: "450px",
-            width: "300px",
+            height: "200px",
+            width: "400px",
             source: source,
             draggable: false,
             allowMoveColumns: false,
             columns: [
-                { text: "Property", width: 200, index: "key", canSortBy: false },
+                { text: "Property", width: 125, index: "key", canSortBy: false },
                 { text: "Value", width: 200, index: "value", canSortBy: false }
             ]
         };
