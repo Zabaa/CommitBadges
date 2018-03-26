4b615d0d44d3b3024a98b675b9fb3f773711bf9a
Zabaa <zaba.luk@gmail.com>
2018-03-27 00:23:59 +0200

    Use react

---
+++ CommitBadges/CommitBadges/src/my-module/view-builders/Components.tsx
@@ -0,0 +1,16 @@
+﻿import * as React from "react";
+
+export interface BadgeCardProps {
+    name: string;
+    points: number;
+}
+
+export class BadgeCard extends React.Component<BadgeCardProps> {
+    render() {
+        return (
+            <div className="badge-panel">
+                <p>{this.props.name} {this.props.points} </p>
+            </div>
+        );
+    }
+}
---
+++ CommitBadges/CommitBadges/src/my-module/view-builders/ReactViewBuilder.tsx
@@ -0,0 +1,14 @@
+﻿import { ViewBuilder } from "./ViewBuilder";
+import Badge from "./../badges/Badge";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as $ from "jquery";
+import { BadgeCard } from "./Components";
+
+export default class ReactViewBuilder implements ViewBuilder {
+    public build(badge: Badge) {
+        ReactDOM.render(
+            <BadgeCard name={badge.name} points={badge.points} />,
+            document.getElementById(badge.name))
+    }
+}
